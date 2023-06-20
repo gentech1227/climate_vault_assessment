139eafb6f1e7c1aa720722398dbbd5394c5644ad
william <wfulton1227@gmail.com>
2023-06-20 05:01:20 -0500

    modified readme file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ClimateLayout } from "./layout";
 import { store } from "./store";
+
 import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
